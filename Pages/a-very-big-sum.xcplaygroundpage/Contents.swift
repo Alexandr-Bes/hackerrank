c948c04cdc4e3643a5edff8a337cdb454b777d31
Alexandr <alex@MacBook-Pro-Alexandr.local>
2018-12-05 23:19:40 +0200

    Initial commit

---
+++ Pages/a-very-big-sum.xcplaygroundpage/Contents.swift
@@ -0,0 +1,18 @@
+
+
+// Prints the sum of the array's elements, some of which may be very large.
+//
+// https://www.hackerrank.com/challenges/a-very-big-sum
+
+import Foundation
+
+
+func aVeryBigSum(ar: [Int]) -> Int {
+    let array = ar
+    var bigSum = 0
+
+    for i in array {
+        bigSum += i
+    }
+    return bigSum
+}
---
+++ Pages/plus-minus.xcplaygroundpage/Contents.swift
@@ -0,0 +1,28 @@
+
+
+// Calculates which fraction of an integer array's elements are positive, negative, and zero.
+//
+// https://www.hackerrank.com/challenges/plus-minus
+
+import Foundation
+
+func plusMinus(arr: [Int]) -> Void {
+    var plus = 0
+    var minus = 0
+    var zero = 0
+
+    for i in arr {
+        if i > 0 {
+            plus += 1
+        } else if i < 0 {
+            minus += 1
+        } else if i == 0 {
+            zero += 1
+        }
+    }
+
+    print(Double(Double(plus) / Double(arr.count)))
+    print(Double(Double(minus) / Double(arr.count)))
+    print(Double(Double(zero) / Double(arr.count)))
+
+}
