0f07a575d6475f8fad45f9a3df2c6033c8b10158
AlexBezkopylnyi <sanok.bes@gmail.com>
2019-02-19 13:33:26 +0200

    Compare the Triplets

---
+++ Pages/compare-the-triplets.xcplaygroundpage/Contents.swift
@@ -0,0 +1,25 @@
+
+// Return an array of two integers denoting the respective comparison points earned by Alice and Bob.
+//
+// https://www.hackerrank.com/challenges/compare-the-triplets/problem
+
+import Foundation
+
+func compareTriplets(a: [Int], b: [Int]) -> [Int] {
+
+    var finalScore = [Int]()
+    var firstScores = 0
+    var secondScores = 0
+
+    for (sc1, sc2) in zip(a, b) {
+
+        if sc1 > sc2 {
+            firstScores += 1
+        } else if sc2 > sc1 {
+            secondScores += 1
+        }
+        finalScore = [firstScores] + [secondScores]
+    }
+    
+    return finalScore
+}
